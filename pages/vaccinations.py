a9f92618548b7d734b62804d192632193b9cd0dd
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2021-04-11 13:40:57 +0000

    Add daily vaccinations chart

--- pages/vaccinations.py
+++ pages/vaccinations.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 import streamlit as st
+import altair as alt
 import numpy as np
+import pandas as pd
 
 from utils.dataset import DataSet
 
@@ -15,10 +17,38 @@ def get_dataset(dummy=f'{datetime.now():%Y-%m-%d}'):
 def get_regions(dataset):
     return np.sort(dataset.df['nome_area'].unique())
 
+def region_df(dataset, region):
+    return dataset.df[dataset.df['nome_area'] == region].sort_index()
+    
+def vac_chart(df, region, loc, height=300):
+    d = df.reset_index()
+    d = pd.melt(d, id_vars=['data_somministrazione'], var_name='type', value_name='doses')
+
+    chart = alt.Chart(d) \
+        .mark_area() \
+        .encode(
+            x=alt.X('data_somministrazione', title=''),
+            y=alt.Y('doses', title=''),
+            color=alt.Color('type', title='')
+        ) \
+        .properties(
+            title={
+                'text': loc.get_text('vac_fig_title_1', region=region),
+                'subtitle': loc.get_text('last_day_title', date=df.index[-1])
+            },
+            width='container',
+            height=height
+        )
+    return chart
+
 def vaccinations_page(loc):
     dataset = get_dataset()
     region = st.sidebar.selectbox(loc.get_text('Region'), get_regions(dataset))
     st.markdown(f'''
     ## {loc.get_text('page_menu_vaccinations')} \u2013 {region}
     ''')
+    df = region_df(dataset, region)
+    df = df[['prima_dose', 'seconda_dose']]
 
+    chart = vac_chart(df, region, loc)
+    st.altair_chart(chart, use_container_width=True)
