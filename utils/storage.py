b3ebf1dc1d6af6b41e08620ef185265ea723cede
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-01-14 16:15:48 +0100

    Save state un url query param

--- utils/storage.py
+++ utils/storage.py
@@ -1,18 +1,13 @@
-import datetime
-import extra_streamlit_components as stx
+import streamlit as st
 
 class Storage:
-
-    def __init__(self, expire_after=datetime.timedelta(days=7)):
-        self.cookie_manager = stx.CookieManager()
-        self.expire_after = expire_after
     
     def get(self, key):
-        return self.cookie_manager.get(key)
+        params = st.experimental_get_query_params()
+        return params.get(key)[0]
 
     def set(self, key, value, expire_after=None):
-        expire_after = expire_after or self.expire_after
-        self.cookie_manager.set(key, str(value), expires_at=datetime.datetime.now() + expire_after)
+        st.experimental_set_query_params(**{key:value})
     
     def __getitem__(self, key):
         return self.get(key)
